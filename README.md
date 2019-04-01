656bc037ecba502d32b3550191c4509dff154c35
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-03-31 23:07:39 -0400

    decent progress

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # GifTastic
+A project for week 6 of my coding bootcamp. Pulls gifs from API.
+
+1. https API link + search criteria + API Key
+2. topicsArray
+3. creat buttons in html for each Array index
+4. Ajax w/ queryURL and "GET"
+5. $("#button)on.("click", generateStill)
+   $("#button").on("click", toggle/animate)
+6. display response.rating under each giphy
+7. .push userInput to topics array
+8. .append button w/ userInput
---
+++ javascript/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+var topicsArray = ["cats","icecream","ariana grande","kim kardashian", "jennifer lawrence"];
+    $("button").on("click", function () {
+        console.log(this);
+        var topic = $(this).attr("data-topic");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            topic + "&api_key=kK9L20nLWIersNTnyEhUOMzC7tO9R31C&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++) {
+                    var gifDiv = $("<div>");
+
+                    var rating = results[i].rating;
+
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    var topicImage = $("<img>");
+                    topicImage.attr("src", results[i].images.fixed_height.url);
+
+                    gifDiv.prepend(p);
+                    gifDiv.prepend(topicImage);
+
+                    $("#gifs-appear-here").prepend(gifDiv);
+                }
+            });
+    });
+});
