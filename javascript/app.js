b02d9d61bbfbadd09ac695ae5237a5ee230ba624
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-04-02 23:25:19 -0400

    updates

--- javascript/app.js
+++ javascript/app.js
@@ -1,34 +1,71 @@
-$(document).ready(function () {
+var topic = $(this).attr("data-topic");
+// var queryURL =
+//   "https://api.giphy.com/v1/gifs/search?q=" +
+//   topic +
+//   "&api_key=kK9L20nLWIersNTnyEhUOMzC7tO9R31C&limit=10";
 
-var topicsArray = ["cats","icecream","ariana grande","kim kardashian", "jennifer lawrence"];
-    $("button").on("click", function () {
-        console.log(this);
-        var topic = $(this).attr("data-topic");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            topic + "&api_key=kK9L20nLWIersNTnyEhUOMzC7tO9R31C&limit=10";
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            .then(function (response) {
-                var results = response.data;
 
-                for (var i = 0; i < results.length; i++) {
-                    var gifDiv = $("<div>");
 
-                    var rating = results[i].rating;
+// $(document).ready(function() {
+  // var topicsArray = [
+  //   "cats",
+  //   "icecream",
+  //   "ariana grande",
+  //   "kim kardashian",
+  //   "jennifer lawrence"
+  // ];
 
-                    var p = $("<p>").text("Rating: " + rating);
+  // $(document).on("click","button",function() {
 
-                    var topicImage = $("<img>");
-                    topicImage.attr("src", results[i].images.fixed_height.url);
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      var results = response.data;
 
-                    gifDiv.prepend(p);
-                    gifDiv.prepend(topicImage);
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div>");
 
-                    $("#gifs-show-here").prepend(gifDiv);
-                }
-            });
+        var rating = results[i].rating;
+
+        var p = $("<p>").text("Rating: " + rating);
+
+        var topicImage = $("<img>");
+        topicImage.attr("src", results[i].images.fixed_height.url);
+
+        gifDiv.prepend(p);
+        gifDiv.prepend(topicImage);
+
+        $("#gifs-show-here").prepend(gifDiv);
+      // }
     });
+
+
+  });
+
+  function makeButtons() {
+    for (var i = 0; i < topicsArray.length; i++) {
+        var button = $("<button>");
+        console.log(topicsArray[i]);
+       
+    }
+  }
+
+  function searchUserGiphy(userInput){
+    console.log(userInput);
+
+  }
+  $("#select-artist").on("click", function(event) {
+    event.preventDefault();
+    var userInput = $("#topic-input").val().trim();
+
+
+
+
+
+
+  makeButtons();
+  searchUserGiphy(userInput);
+});
 });
