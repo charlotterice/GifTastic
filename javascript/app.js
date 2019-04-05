328d44ba4da19b7b72e2a47450d64697c2bf6909
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-04-04 23:33:57 -0400

    something wrong with this, showing window

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,11 @@
-var topicsArray = [
-  "cats",
-  "icecream",
-  "ariana grande",
-  "kim kardashian",
-  "jennifer lawrence"
-];
-
 $(document).ready(function() {
+  var topicsArray = [
+    "cats",
+    "icecream",
+    "ariana grande",
+    "kim kardashian",
+    "jennifer lawrence"
+  ];
   function makeButtons() {
     $("#buttons-div").empty();
     for (var i = 0; i < topicsArray.length; i++) {
@@ -19,34 +18,34 @@ $(document).ready(function() {
     }
   }
   function displayGifs() {
-    var topic;
+    console.log(this);
+    var topic = $(this).attr("data-name");
     var queryURL =
       "https://api.giphy.com/v1/gifs/search?q=" +
       topic +
       "&api_key=kK9L20nLWIersNTnyEhUOMzC7tO9R31C&limit=10";
-    var topic = $(this).attr("data-name");
-    console.log(this);
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
+      console.log(response.data);
       var results = response.data;
       console.log(results);
       for (var i = 0; i < results.length; i++) {
         var gifDiv = $("<div>");
+        var topicImage = $("<img>");
+        topicImage.attr("src", results[i].images.fixed_height_still.url);
+        topicImage.attr("data-state", "still");
+        topicImage.addClass("gif");
+        topicImage.attr("data-animate", results[i].images.fixed_height.url);
         var rating = results[i].rating;
         var p = $("<p>").text("Rating" + rating);
-        var topicImage = $("<img>");
-        topicImage.attr("src", results[i].images.fixed_height.url);
         gifDiv.prepend(p);
         gifDiv.prepend(topicImage);
         $("gifs-show-here").prepend(gifDiv);
       }
     });
   }
-  $("button").on("click", function(){
-      displayGifs();
-  })
 
   $("#select-input").on("click", function() {
     var topic = $("#topic-input")
@@ -55,4 +54,20 @@ $(document).ready(function() {
     topicsArray.push(topic);
     makeButtons();
   });
+
+  $(document).on("click", ".gif", function() {
+    var state = $(this).attr("data-state");
+    if (state == "still") {
+      $(this).attr("src", $(this).data("animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).data("still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+
+  $(document).on("click", function() {
+    displayGifs();
+  });
+  makeButtons();
 });
